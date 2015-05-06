513e13dc01b8ab121b8f55c0d9d347aff1df845e
Ashwin Kumar <ash.nkumar@gmail.com>
2015-05-06 15:50:47 -0700

    Uber request popup in progress ...

--- uberhxthon/uberhxthon/DeliveryView/Controllers/RequestUberPopup/RequestUberPopupViewController.m
+++ uberhxthon/uberhxthon/DeliveryView/Controllers/RequestUberPopup/RequestUberPopupViewController.m
@@ -15,15 +15,19 @@
 
 @interface RequestUberPopupViewController ()
 @property (strong, nonatomic) RTSpinKitView *requestingSpinner;
+@property (strong, nonatomic) UIView *checkView;
 @end
 
 @implementation RequestUberPopupViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"ViewDidLoad frame is: %@", NSStringFromCGRect(self.view.frame));
+    NSLog(@"ViewDidLoad view: %@", self.view);
     
     self.view.layer.cornerRadius = 5;
     self.view.layer.masksToBounds = YES;
+    
     
     self.requestingSpinner = [[RTSpinKitView alloc] initWithStyle:RTSpinKitViewStyleArc
                                                             color:[AppConstants cashwinGreen]];
@@ -33,18 +37,37 @@
 - (void)viewWillLayoutSubviews
 {
     [super viewWillLayoutSubviews];
-    CGRect newFrame = CGRectMake(380, 0, 250, 60);
-    self.view.frame = newFrame;
+    NSLog(@"ViewWillLayoutSubviews frame is: %@", NSStringFromCGRect(self.view.frame));    
+    self.view.frame = CGRectMake(380, 0, 250, 60);
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    UIImageView *checkView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"myCheckBox"]];
-    checkView.frame = CGRectMake(10, 10, 40, 40);
-    [self.view addSubview:checkView];
-    
+//    NSLog(@"ViewWillAppear frame is: %@", NSStringFromCGRect(self.view.frame));
     [self.view addSubview:self.requestingSpinner];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self performSelector:@selector(uberRequestComplete) withObject:self afterDelay:3.0];
+}
+
+- (void)uberRequestComplete
+{
+//    
+//    self.checkView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"myCheckBox"]];
+//    self.checkView.frame = CGRectMake(10, 10, 40, 40);
+//    
+//    [self.requestingSpinner removeFromSuperview];
+//    self.requestStatusLabel.text = @"Uber Requested!";
+//    [self.view addSubview:self.checkView];
+    
+    UIView *newView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
+    newView.backgroundColor = [UIColor redColor];
+//    [self.view removeFromSuperview];
+
 }
 
 - (void)didReceiveMemoryWarning {
